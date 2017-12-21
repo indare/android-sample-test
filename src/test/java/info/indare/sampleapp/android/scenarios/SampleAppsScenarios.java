70c984f50976d2ac06dc3223063136be4a1fced8
Masashi Arino <masashi.arino@gmail.com>
2017-12-21 17:24:37 +0900

    file init

---
+++ src/test/java/info/indare/sampleapp/android/scenarios/SampleAppsScenarios.java
@@ -0,0 +1,11 @@
+package info.indare.sampleapp.android.scenarios;
+
+import com.thoughtworks.selenium.ScreenshotListener;
+import org.testng.annotations.Listeners;
+
+@Listeners({ScreenshotListener.class})
+public class SampleAppsScenarios {
+
+
+
+}
